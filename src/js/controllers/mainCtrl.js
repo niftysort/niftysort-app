a84a486768cde64b5fdb4b07936f12ad3d42ff34
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-15 23:14:10 -0700

    controller convert es5 to es6

--- src/js/controllers/mainCtrl.js
+++ src/js/controllers/mainCtrl.js
@@ -1,39 +1,33 @@
-
 app.controller('mainCtrl', function(categoryService, graphService, productService, swalService, cardService, $scope){
-  console.log('main controller loaded!');
-
 
   //SET NUM OF PLACHOLDER CARDS
   $scope.numOfPlacholders = 10;
-  $scope.getPlaceholders = function(num){
+  $scope.getPlaceholders = num => {
     return new Array(num);
   }
 
-
-  // ASSIGN SLIDER OPTIONS
-
   //GET ALL CATEGORIES FROM BACKEND ON PAGE LOAD
   categoryService.retrieveAllCategories()
-  .then(function(resp) {
-    $scope.categoryNames = resp.data.map(function(val) {
+  .then( resp => {
+    $scope.categoryNames = resp.data.map( val => {
       return val.name;
     })
     console.log('Possible categories: ', $scope.categoryNames);
-  }, function(err) {
+  }, err => {
   	swalNoCategories();
   })
 
   //GET SPECIFIC CATEGORY FROM INPUT
-  $scope.getCategory = function(searchCategory) {
+  $scope.getCategory = searchCategory => {
     categoryService.retrieveCategory(searchCategory)
-    .then(function(resp) {
+    .then( resp => {
       $scope.category = {
         id: resp.data._id,
         name: resp.data.name
       };
       categoryService.setCursorAttribute();
 
-    }, function(err) {
+    }, err => {
     	$scope.category = null;
       // TODO: Handle error with UI, notify user that attribute is not recommended.
       // swalErrorAttribute()
@@ -41,7 +35,7 @@ app.controller('mainCtrl', function(categoryService, graphService, productServic
     })
   };
 
-  $scope.newCategory = function() {
+  $scope.newCategory = () => {
     $scope.category = categoryService.reset();
     $scope.searchCategory = categoryService.clearInput();
     $scope.autoComplete = '';
@@ -64,118 +58,115 @@ app.controller('mainCtrl', function(categoryService, graphService, productServic
     swalService.noCategory();
   }
 
-//PASS OPTIONS TO HIGHCHARTS
-var chart = new Highcharts.Chart({
-  chart: {
-    renderTo: 'container',
-    type: 'scatter',
-    zoomType: ''
-  },
-  credits: {
-    enabled: false
-  },
-  title: {
-    text: null
-  },
-  subtitle: {
-    text: null
-  },
-  xAxis: {
-    visible: false,
-    title: {
-      enabled: true,
-      text: 'Height (cm)'
+  //PASS OPTIONS TO HIGHCHARTS
+  let chart = new Highcharts.Chart({
+    chart: {
+      renderTo: 'container',
+      type: 'scatter',
+      zoomType: ''
     },
-    startOnTick: true,
-    endOnTick: true,
-    showLastLabel: true
-  },
-  yAxis: {
-    visible: false,
+    credits: {
+      enabled: false
+    },
     title: {
-      text: 'Weight (kg)'
-    }
-  },
-  legend: {
-    enabled: false,
-    layout: 'vertical',
-    align: 'left',
-    verticalAlign: 'top',
-    x: 100,
-    y: 70,
-    floating: true,
-    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-    borderWidth: 1
-  },
-  plotOptions: {
-    scatter: {
-      marker: {
-        radius: 5,
+      text: null
+    },
+    subtitle: {
+      text: null
+    },
+    xAxis: {
+      visible: false,
+      title: {
+        enabled: true,
+        text: 'Height (cm)'
+      },
+      startOnTick: true,
+      endOnTick: true,
+      showLastLabel: true
+    },
+    yAxis: {
+      visible: false,
+      title: {
+        text: 'Weight (kg)'
+      }
+    },
+    legend: {
+      enabled: false,
+      layout: 'vertical',
+      align: 'left',
+      verticalAlign: 'top',
+      x: 100,
+      y: 70,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+      borderWidth: 1
+    },
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 5,
+          states: {
+            hover: {
+              enabled: false,
+              lineColor: 'rgb(100,100,100)'
+            }
+          }
+        },
         states: {
           hover: {
-            enabled: false,
-            lineColor: 'rgb(100,100,100)'
+            marker: {
+              enabled: true
+            }
           }
-        }
-      },
-      states: {
-        hover: {
-          marker: {
-            enabled: true
+        },
+        tooltip: {
+          headerFormat: '',
+          pointFormatter() {
+            let name = (this.name.length > 40) ? this.name.substring(0,40).trim() +'...' : name;
+            let price = this.xR.toFixed(2)
+            let rating = this.rating.toFixed(2);
+            return `<h1><b>${name}</b></h1><br>Price: $${price} - Rating: <strong>${rating}</strong>`
           }
-        }
-      },
-      tooltip: {
-        headerFormat: '',
-        pointFormatter: function() {
-          var name = (this.name.length > 40) ? this.name.substring(0,40).trim() +'...' : name;
-          var price = this.xR.toFixed(2)
-          var rating = this.rating.toFixed(2);
-          return `<h1><b>${name}</b></h1><br>Price: $${price} - Rating: <strong>${rating}</strong>`
-        }
-      },
-      point: {
-        events: {
-          click: function() {
-            var id = this.id;
-            graphService.toggleSelected(id, $scope.topProducts);
-            cardService.scrollTo(id);
-          },
+        },
+        point: {
+          events: {
+            click() {
+              let id = this.id;
+              graphService.toggleSelected(id, $scope.topProducts);
+              cardService.scrollTo(id);
+            },
+          }
         }
       }
-    }
-  },
-  series: [{}]
-});
-
+    },
+    series: [{}]
+  });
 
   //MARKER AND CARD CLICK HANDLER
-  $scope.renderTest = function(id) {
+  $scope.renderTest = id => {
     graphService.toggleSelected(id, $scope.topProducts);
   }
 
   //get products of category from backend and send to cache
-  $scope.getGraph = function(attribute) {
+  $scope.getGraph = attribute => {
     $scope.attribute = attribute;
   	graphService.retrieveGraphData($scope.category.id, attribute)
-  	.then(function(resp) {
-      console.log(resp.data);
+  	.then( resp => {
       cacheCategoryData(resp.data);
-  	}, function(err) {
+  	}, err => {
   		$scope.data = [];
   		console.log('err ',err);
   	})
   }
 
-  $scope.autoFill= function(categoryInput) {
-
+  $scope.autoFill = categoryInput => {
     // on right arrow key up
     if (event.keyCode === 39) {
       this.searchCategory = $scope.autoComplete;
     }
 
     if (categoryInput) {
-      var filteredCategories = $scope.categoryNames.filter(function(val) {
+      let filteredCategories = $scope.categoryNames.filter( val => {
         return val.slice(0, categoryInput.length) === categoryInput;
       });
 
@@ -185,17 +176,13 @@ var chart = new Highcharts.Chart({
     }
   }
 
-
   //only invoked upon attribute query, output is scope.products and scope.topProducts
   function cacheCategoryData(category) {
     $scope.products = category.values;
-    var products = category.values;
-    var productsInfo = getProductInfo(products);
-
-    var topProducts = graphService.assignPointProperties(productsInfo);
-
+    let products = category.values;
+    let productsInfo = getProductInfo(products);
+    let topProducts = graphService.assignPointProperties(productsInfo);
     $scope.topProducts = productService.removeHiddenCharacters(topProducts);
-
     renderGraph($scope.topProducts);
     initializeSlider(productsInfo.minX, productsInfo.maxX);
   }
@@ -204,31 +191,28 @@ var chart = new Highcharts.Chart({
   //ON SLIDER CHANGE TAKE IN SLIDER MIN MAX AND RE-CALC RANGE
   function getRangeTopProducts(minPrice, maxPrice) {
     chart.series[0].setData([{}]);
-    var products = $scope.products;
-    var productsInfo = getProductInfo(products, minPrice, maxPrice);
-
-    var topProducts = graphService.assignPointProperties(productsInfo);
-
+    let products = $scope.products;
+    let productsInfo = getProductInfo(products, minPrice, maxPrice);
+    let topProducts = graphService.assignPointProperties(productsInfo);
     $scope.topProducts = productService.removeHiddenCharacters(topProducts);
-
     renderGraph($scope.topProducts);
   }
 
   function getProductInfo(products, minPrice, maxPrice) {
-    var permittedProducts = productService.removeZeroValueProducts(products);
-    var minX = productService.getMinX(permittedProducts);
-    var maxX = productService.getMaxX(permittedProducts);
-
+    let permittedProducts = productService.removeZeroValueProducts(products);
+    let minX = productService.getMinX(permittedProducts);
+    let maxX = productService.getMaxX(permittedProducts);
+    let rangedProducts;
     if (minPrice && maxPrice) {
-      var rangedProducts = productService.getProductsInRange(permittedProducts, minPrice, maxPrice);
+      rangedProducts = productService.getProductsInRange(permittedProducts, minPrice, maxPrice);
     } else {
-      var rangedProducts = productService.getProductsInRange(permittedProducts, minX.xR, maxX.xR);
+      rangedProducts = productService.getProductsInRange(permittedProducts, minX.xR, maxX.xR);
     }
 
-    var maxY = productService.getMaxY(permittedProducts);
-    var sortedProductsByRating = productService.sortCachedData(rangedProducts, maxX, maxY);
-    var desiredNumResults = (permittedProducts.length > 10) ? 10 : sortedProductsByRating.length;
-    var topProducts = productService.getTopResults(sortedProductsByRating, desiredNumResults);
+    let maxY = productService.getMaxY(permittedProducts);
+    let sortedProductsByRating = productService.sortCachedData(rangedProducts, maxX, maxY);
+    let desiredNumResults = (permittedProducts.length > 10) ? 10 : sortedProductsByRating.length;
+    let topProducts = productService.getTopResults(sortedProductsByRating, desiredNumResults);
 
     return {
       desiredNumResults,
@@ -243,8 +227,6 @@ var chart = new Highcharts.Chart({
   }
 
   function initializeSlider(minX, maxX) {
-    console.log(minX, maxX);
-
     $scope.slider = {
       min: minX.xR,
       max: maxX.xR,
@@ -252,24 +234,22 @@ var chart = new Highcharts.Chart({
         step: 0.01,
         precision: 2,
         floor: minX.xR,
-        translate: function(value) {
+        translate(value) {
           return '$' + value.toFixed(2);
         },
         hideLimitLabels: false,
         disabled: false,
         minRange: 5,
         noSwitching: true,
-        onEnd: function() {
+        onEnd() {
           getRangeTopProducts($scope.slider.min, $scope.slider.max);
         }
       }
     };
-    
   }
 
   function renderGraph(products) {
     chart.series[0].setData(products, false);
     chart.redraw();
   }
-
 });
